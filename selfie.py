95d712f029232ecc28304b2a2f92b64433c4e3c4
Egor Urvanov <hedgehogues@bk.ru>
2019-08-19 13:46:20 +0300

    add accuracy

--- selfie.py
+++ selfie.py
@@ -136,7 +136,7 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
@@ -229,6 +229,7 @@ def test():
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
+            print(test_loss, (targets == predicted).numpy().sum() / predicted.shape[0])
 
 
 for epoch in range(0, 0+200):
